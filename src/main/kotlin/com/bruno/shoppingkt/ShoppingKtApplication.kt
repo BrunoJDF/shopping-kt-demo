437f112bc07f40d91468f4126312b948d177def6
Bruno Josue Diaz Floreano <PE01026487@prosegur.com>
2024-09-01 19:04:15 -0500

    chore: first commit

---
+++ src/main/kotlin/com/bruno/shoppingkt/ShoppingKtApplication.kt
@@ -0,0 +1,11 @@
+package com.bruno.shoppingkt
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ShoppingKtApplication
+
+fun main(args: Array<String>) {
+    runApplication<ShoppingKtApplication>(*args)
+}
---
+++ src/test/kotlin/com/bruno/shoppingkt/ShoppingKtApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.bruno.shoppingkt
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ShoppingKtApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
