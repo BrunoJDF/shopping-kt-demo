e6049ca6e960813e00328f15a7dc78629d124dbf
Bruno Josue Diaz Floreano <brunjo12@hotmail.com>
2024-09-03 01:35:46 -0500

    feat: controller

--- src/main/kotlin/com/bruno/shoppingkt/infrastructure/controller/ProductController.kt
+++ src/main/kotlin/com/bruno/shoppingkt/infrastructure/controller/ProductController.kt
@@ -1,15 +1,17 @@
 package com.bruno.shoppingkt.infrastructure.controller
 
+import com.bruno.shoppingkt.application.response.ProductResponse
+import com.bruno.shoppingkt.domain.ProductService
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/products")
-class ProductController {
+class ProductController(var productService: ProductService) {
 
     @GetMapping
-    fun getAllProducts(): String {
-        return "All products"
+    fun getAllProducts(): List<ProductResponse> {
+        return productService.getAllProducts()
     }
 }
