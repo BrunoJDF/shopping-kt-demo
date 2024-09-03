1bd780f5101255628835ce8a6fb34a4a338d5c09
Bruno Josue Diaz Floreano <brunjo12@hotmail.com>
2024-09-03 01:01:47 -0500

    feat: move controller to package controller

--- src/main/kotlin/com/bruno/shoppingkt/infrastructure/controller/ProductController.kt
+++ src/main/kotlin/com/bruno/shoppingkt/infrastructure/controller/ProductController.kt
@@ -1,4 +1,4 @@
-package com.bruno.shoppingkt.infrastructure
+package com.bruno.shoppingkt.infrastructure.controller
 
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
