b0d86a7166408934637b04008150520e3d8d2a6c
Bruno Josue Diaz Floreano <brunjo12@hotmail.com>
2024-09-07 11:22:39 -0500

    feat: add auto-config to datasource

--- src/main/kotlin/com/bruno/shoppingkt/domain/Product.kt
+++ src/main/kotlin/com/bruno/shoppingkt/domain/Product.kt
@@ -1,12 +1,9 @@
 package com.bruno.shoppingkt.domain
 
-import jakarta.persistence.Column
-import jakarta.persistence.Entity
-import jakarta.persistence.GeneratedValue
-import jakarta.persistence.GenerationType
-import jakarta.persistence.Id
+import jakarta.persistence.*
 
-@Entity(name = "products")
+@Entity
+@Table(name = "product", schema = "shopping_cart")
 class Product(
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
